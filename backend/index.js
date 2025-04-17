f33d3b633367824b18d3a0c1b9797ad1f964fe35
unknown <vishvam@example.com>
2025-04-17 15:39:18 +0530

    fix: enable CORS for frontend

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,15 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const cron = require("node-cron")
+const cron = require("node-cron");
 // const { connect } = require('./backend/V1/api');
 const app = express();
-const path = require("path")
+const path = require("path");
 // app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://earnest-pithivier-99c33c.netlify.app/",
     credentials: true,
 }));
 
